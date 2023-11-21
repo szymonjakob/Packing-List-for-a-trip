b1618bbadfc2f9a06b6c9029142b0f421dec96cc
szymonjakob <szymon@studiosj.pl>
2023-11-21 19:22:41 +0100

    Refactored to files

---
+++ src/components/App.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleRemoveItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearAll() {
+    const confirmed = window.confirm(
+      "Do you really want to clear all the list?"
+    );
+
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onRemoveItem={handleRemoveItem}
+        onToggleItem={handleToggleItem}
+        onClearAll={handleClearAll}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
